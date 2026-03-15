526e9752defc830d2a5df59225bed834b5e3b989
codderrrrr <saadwaqar211@gmail.com>
2026-03-16 01:28:54 +0500

    Updated OCR code

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import gradio as gr
+import numpy as np
 from paddleocr import PaddleOCRVL
 
 # Load model once
 pipeline = PaddleOCRVL()
 
 def run_ocr(image):
+    # Convert PIL image to numpy array
+    image_np = np.array(image)
 
-    result = pipeline.predict(image)
+    # Run OCR
+    result = pipeline.predict(image_np)
 
+    # Format result
     text = ""
-
     for r in result:
         text += str(r) + "\n"
 
     return text
 
-
 demo = gr.Interface(
     fn=run_ocr,
-    inputs=gr.Image(type="pil"),
+    inputs=gr.Image(type="pil"),  # still receives PIL from Gradio
     outputs="text",
     title="Handwriting OCR"
 )
