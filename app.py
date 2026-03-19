9063e62414cb71a2550b5b2ffbbc51ce20a93a22
codderrrrr <saadwaqar211@gmail.com>
2026-03-20 01:41:47 +0500

    Made function run ocr async

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 import gradio as gr
 import numpy as np
 from paddleocr import PaddleOCRVL
+import asyncio
 
-# Initialize PaddleOCRVL
 pipeline = PaddleOCRVL(
     pipeline_version="v1.5",
     use_layout_detection=True,
 )
 
-def run_ocr(image):
-    """
-    image: PIL.Image from Gradio
-    """
-    # Convert PIL image to NumPy array
+async def run_ocr(image):
     img_np = np.array(image)
-    output = pipeline.predict(img_np)
-    
-    # Convert output to string for display
+    output = await asyncio.to_thread(
+        pipeline.predict, img_np
+    )
     return str(output)
 
-# Gradio interface
 iface = gr.Interface(
     fn=run_ocr,
-    inputs=gr.Image(type="pil"),  # Pass PIL image from Gradio
+    inputs=gr.Image(type="pil"),  
     outputs="text",
     title="PaddleOCRVL on Hugging Face Spaces",
     description="Upload an image and get OCR output using PaddleOCRVL",
